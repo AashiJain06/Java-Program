8e308f54a85595a984e47b9b40a12ae5ed021f85
Aashi Jain <aashijain064@gmail.com>
2024-07-15 21:32:51 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,19 @@
+public class Pattern3 {
+  public static void main(String[] args) {
+    int j, i;
+    for (i = 1; i <= 5; i++) {
+      for (j = 1; j <= i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    for (i = 1; i <= 4; i++) {
+      for (j = 4; j >= i; j--) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
+  }
+
+}
