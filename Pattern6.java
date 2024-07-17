b382bdf4b2af9683d6e05aef0719d3a9d846ad41
Aashi Jain <aashijain064@gmail.com>
2024-07-17 22:19:43 +0530

    Add files via upload

---
+++ Pattern6.java
@@ -0,0 +1,16 @@
+public class Pattern6 {
+  public static void main(String[] args) {
+    int i, j, k;
+    for (i = 1; i <= 5; i++) {
+      for (j = 5; j >= i; j--) {
+        System.out.print(" ");
+      }
+      for (k = 1; k <= i; k++) {
+        System.err.print(" *");
+      }
+      System.out.println();
+    }
+
+  }
+
+}
