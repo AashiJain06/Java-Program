5db05537128216828593b7e61f943103273746c7
Aashi Jain <aashijain064@gmail.com>
2024-07-17 22:22:15 +0530

    Add files via upload

--- ReverseString.java
+++ ReverseString.java
@@ -4,12 +4,13 @@ public class ReverseString {
   public static void main(String[] args) {
 
     Scanner sc = new Scanner(System.in);
-    String name, rev;
+    String name;
+    String rev = "";
     System.out.println("enter string");
     name = sc.next();
-    int n, i;
-    n = name.Length;
-    for (i = n - 1; i >= 0; i--) {
+    int n;
+    n = name.length();
+    for (int i = n - 1; i >= 0; i--) {
       rev = rev + name.charAt(i);
     }
     System.out.println(rev);
