a43cb9d24fdc00437061f4de90b43a78a0d6ff0e
Aashi Jain <aashijain064@gmail.com>
2024-07-17 22:21:12 +0530

    Add files via upload

---
+++ Pattern8.java
@@ -0,0 +1,18 @@
+public class Pattern8 {
+  public static void main(String[] args) {
+    int i, j, k, l;
+    for (i = 0; i <= 5; i++) {
+      for (j = 1; j <= i; j++) {
+        System.out.print(" ");
+      }
+      for (k = 5; k >= i; k--) {
+        System.out.print("*");
+      }
+      for (l = 4; l >= i; l--) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+
+}
