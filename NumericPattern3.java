b829269f44b3eca20de3cca80f88f83ee6ddfdac
Aashi Jain <aashijain064@gmail.com>
2024-07-18 22:27:26 +0530

    Add files via upload

---
+++ NumericPattern3.java
@@ -0,0 +1,14 @@
+public class NumericPattern3 {
+  public static void main(String[] args) {
+    int i, j, count;
+    count = 1;
+    for (i = 0; i <= 5; i++) {
+      for (j = 1; j <= i; j++) {
+        System.out.print(" " + count);
+        count++;
+      }
+      System.out.println();
+    }
+  }
+
+}
