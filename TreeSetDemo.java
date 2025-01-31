397792b29cf0d6bd90e16aba5f8ced04d8b92ac4
Aashi Jain <aashijain064@gmail.com>
2025-01-31 22:52:57 +0530

    Add files via upload

---
+++ TreeSetDemo.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class StackDemo {
+  public static void main(String[] args) {
+    TreeSet<Integer> t = new TreeSet<Integer>();
+    TreeSet<String> s = new TreeSet<String>();
+    t.add(10);
+    t.add(20);
+    t.add(40);
+    t.add(12);
+    System.out.println(t);
+    s.add("riya");
+    s.add("aashi");
+    s.add("anurag");
+    s.add("rekha");
+    System.out.println(s);
+
+  }
+
+}
